968cd8239556373dd091eaa532e7569e3ad61a19
Ilgara Yusifzada <ilgarayusifzada@gmail.com>
2025-08-16 20:17:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -649,6 +649,7 @@ def show_reports(tracker):
 def show_settings(tracker):
     """Show system settings"""
     st.header("System Settings")
+    st.write(tracker.known_names)
     
     # Model settings
     st.subheader("Recognition Settings")
